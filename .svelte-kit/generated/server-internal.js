29672e2d41ff6c5f9df69814b70187f79cc03e4c
Kwandapchumba <kwandapchumba@gmail.com>
2023-02-08 18:22:28 +0300

    added omit dev dependencies on prod

--- .svelte-kit/generated/server-internal.js
+++ .svelte-kit/generated/server-internal.js
@@ -3,7 +3,7 @@ import root from './root.svelte';
 import { set_building, set_paths, set_version } from '../../node_modules/@sveltejs/kit/src/runtime/shared.js';
 
 set_paths({"base":"","assets":""});
-set_version("1675866362294");
+set_version("1675866853140");
 
 export const options = {
 	csp: {"mode":"auto","directives":{"upgrade-insecure-requests":false,"block-all-mixed-content":false},"reportOnly":{"upgrade-insecure-requests":false,"block-all-mixed-content":false}},
