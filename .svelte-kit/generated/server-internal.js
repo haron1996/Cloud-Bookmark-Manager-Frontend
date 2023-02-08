8daad8bed6c137dbf9252a90774f0b170b6ff798
Kwandapchumba <kwandapchumba@gmail.com>
2023-02-08 18:26:58 +0300

    added --omit-dev flag

--- .svelte-kit/generated/server-internal.js
+++ .svelte-kit/generated/server-internal.js
@@ -3,7 +3,7 @@ import root from './root.svelte';
 import { set_building, set_paths, set_version } from '../../node_modules/@sveltejs/kit/src/runtime/shared.js';
 
 set_paths({"base":"","assets":""});
-set_version("1675869764930");
+set_version("1675869861421");
 
 export const options = {
 	csp: {"mode":"auto","directives":{"upgrade-insecure-requests":false,"block-all-mixed-content":false},"reportOnly":{"upgrade-insecure-requests":false,"block-all-mixed-content":false}},
