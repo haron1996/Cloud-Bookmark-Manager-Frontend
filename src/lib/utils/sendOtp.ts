03e908b706f4962ced56d9af8f1e5a99ca941792
Kwandapchumba <kwandapchumba@gmail.com>
2023-02-09 21:37:10 +0300

    removed credentials from getotp request

--- src/lib/utils/sendOtp.ts
+++ src/lib/utils/sendOtp.ts
@@ -14,11 +14,10 @@ export const sendOTP = async (s: Session) => {
 		method: 'POST', // *GET, POST, PUT, DELETE, etc.
 		mode: 'cors', // no-cors, *cors, same-origin
 		cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-		credentials: 'include', // include, *same-origin, omit
+		//credentials: 'include', // include, *same-origin, omit
 		headers: {
-			'Content-Type': 'application/json',
-			authorization: `Bearer${s.access_token}`
-			// 'Content-Type': 'application/x-www-form-urlencoded',
+			'Content-Type': 'application/json;charset=UTF-8'
+			//authorization: `Bearer${s.access_token}`
 		},
 		redirect: 'follow', // manual, *follow, error
 		referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
