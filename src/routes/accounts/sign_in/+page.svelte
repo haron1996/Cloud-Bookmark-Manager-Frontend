1da98cfe59a0c915d61b22424fcd387169aa5005
Kwandapchumba <kwandapchumba@gmail.com>
2023-02-11 17:47:47 +0300

    sign in page design improvements

--- src/routes/accounts/sign_in/+page.svelte
+++ src/routes/accounts/sign_in/+page.svelte
@@ -58,60 +58,53 @@
 <Mainnav />
 
 <div class="content">
-	<div class="container">
-		<form>
-			<div class="heading">
-				<h3 class="sign_in_heading">Hey 👋, sign in to your account</h3>
+	<form>
+		<div class="heading">
+			<h3 class="sign_in_heading">Hey 👋, sign in to your account</h3>
+		</div>
+		<div class="inputs">
+			<div class="email" class:invalid_email={$invalid_email} class:email_required={emailIsEmpty}>
+				<label for="email">Email address</label>
+				<input
+					type="email"
+					name="email"
+					id="email"
+					placeholder="Enter your email address"
+					spellcheck="false"
+					bind:value={email}
+				/>
+				<span class="required">Required</span>
+				<span class="invalid">Invalid</span>
 			</div>
-			<div class="inputs">
-				<div class="email" class:invalid_email={$invalid_email} class:email_required={emailIsEmpty}>
-					<label for="email">Email address</label>
-					<input
-						type="email"
-						name="email"
-						id="email"
-						placeholder="Enter your email address"
-						spellcheck="false"
-						bind:value={email}
-					/>
-					<span class="required">Required</span>
-					<span class="invalid">Invalid</span>
-				</div>
-				<div
-					class="password"
-					class:invalid_password={$invalid_password}
-					class:password_required={passwordIsEmpty}
-				>
-					<label for="password">Password</label>
-					<input
-						type="password"
-						name="password"
-						id="password"
-						placeholder="Enter your password"
-						on:contextmenu|stopPropagation={stop_propagation}
-						bind:value={password}
-					/>
-					<span class="required">Required</span>
-					<span class="invalid">Invalid</span>
-				</div>
+			<div
+				class="password"
+				class:invalid_password={$invalid_password}
+				class:password_required={passwordIsEmpty}
+			>
+				<label for="password">Password</label>
+				<input
+					type="password"
+					name="password"
+					id="password"
+					placeholder="Enter your password"
+					on:contextmenu|stopPropagation={stop_propagation}
+					bind:value={password}
+				/>
+				<span class="required">Required</span>
+				<span class="invalid">Invalid</span>
 			</div>
-			<div class="button">
-				<button type="submit" on:click|preventDefault|stopPropagation={submitCreds}>
-					<span>{submitting ? 'Processing...' : 'Sign in to my account'}</span>
-				</button>
-			</div>
-			<div class="sign_up_instead">
-				<span
-					class="sign_up_heading"
-					on:click|preventDefault|stopPropagation={signupInstead}
-					on:keyup>Don't have an account? Create my free account.</span
-				>
-			</div>
-		</form>
-	</div>
-	<div class="gif">
-		<img src={helloGif} alt="hello" />
-	</div>
+		</div>
+		<div class="button">
+			<button type="submit" on:click|preventDefault|stopPropagation={submitCreds}>
+				<span>{submitting ? 'Processing...' : 'Sign in to my account'}</span>
+			</button>
+		</div>
+		<div class="sign_up_instead">
+			<span class="sign_up_heading" on:click|preventDefault|stopPropagation={signupInstead} on:keyup
+				>Don't have an account? Create my free account.</span
+			>
+		</div>
+	</form>
 </div>
 
 <style lang="scss">
@@ -124,225 +117,218 @@
 		overflow: auto;
 		display: flex;
 		align-items: center;
+		justify-content: center;
 
-		.container {
-			flex: 1;
-			height: 100%;
+		form {
+			width: 30%;
 			display: flex;
+			flex-direction: column;
 			align-items: center;
-			justify-content: center;
+			gap: 3em;
+			background-color: white;
 
-			form {
-				min-width: 60%;
+			.heading {
+				text-align: left;
+				width: 100%;
+				display: flex;
+				align-items: center;
+
+				h3 {
+					font-size: 2rem;
+					font-weight: 500;
+					font-family: 'Product Sans Medium', sans-serif;
+					color: #181d31;
+				}
+			}
+
+			.inputs {
+				width: 100%;
 				display: flex;
 				flex-direction: column;
-				align-items: center;
-				gap: 3em;
-				background-color: white;
+				gap: 2em;
 
-				.heading {
-					text-align: left;
-					width: 100%;
-					display: flex;
-					align-items: center;
-
-					h3 {
-						font-size: 2rem;
-						font-weight: 500;
-						font-family: 'Product Sans Medium', sans-serif;
-						color: #181d31;
-					}
-				}
-
-				.inputs {
-					width: 100%;
+				.email {
 					display: flex;
 					flex-direction: column;
-					gap: 2em;
+					gap: 0.2em;
 
-					.email {
-						display: flex;
-						flex-direction: column;
-						gap: 0.2em;
-
-						label {
-							font-size: 1.3rem;
-							font-family: 'Arial CE', sans-serif;
-							color: $text-color-regular;
-						}
-
-						input[type='email'] {
-							padding: 0.5em;
-							border: 0.1rem solid $border-color-regular;
-							outline: none;
-							transition: all 200ms ease-in-out;
-							font-family: 'Arial CE', sans-serif;
-							border-radius: 0.3rem;
-
-							&::placeholder {
-								color: $text-color-medium;
-								font-family: 'Arial CE', sans-serif;
-							}
-
-							&:hover {
-								box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px,
-									rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;
-							}
-
-							&:focus {
-								box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px,
-									rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;
-							}
-						}
-
-						span {
-							font-family: 'Arial CE', sans-serif;
-							font-size: 1.3rem;
-							font-weight: 500;
-							color: $form_error_red;
-							display: none;
-						}
-					}
-
-					.email_required {
-						input[type='email'] {
-							border-color: $form_error_red;
-						}
-
-						span.required {
-							display: inline-block;
-						}
-					}
-
-					.invalid_email {
-						input[type='email'] {
-							border-color: $form_error_red;
-						}
-
-						span.invalid {
-							display: inline-block;
-						}
-					}
-
-					.password {
-						display: flex;
-						flex-direction: column;
-						gap: 0.2em;
-						font-family: 'Arial CE', sans-serif;
-
-						label {
-							font-size: 1.3rem;
-							font-family: 'Arial CE', sans-serif;
-							color: $text-color-regular;
-						}
-
-						input[type='password'] {
-							padding: 0.5em;
-							border: 0.1rem solid $border-color-regular;
-							outline: none;
-							transition: all 200ms ease-in-out;
-							font-family: 'Arial CE', sans-serif;
-							border-radius: 0.3rem;
-
-							&::placeholder {
-								color: $text-color-medium;
-								font-family: 'Arial CE', sans-serif;
-							}
-
-							&:hover {
-								box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px,
-									rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;
-							}
-
-							&:focus {
-								box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px,
-									rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;
-							}
-						}
-
-						span {
-							font-family: 'Arial CE', sans-serif;
-							font-size: 1.3rem;
-							font-weight: 500;
-							color: $form_error_red;
-							display: none;
-						}
-					}
-
-					.password_required {
-						input[type='password'] {
-							border-color: $form_error_red;
-						}
-
-						span.required {
-							display: inline-block;
-						}
-					}
-
-					.invalid_password {
-						input[type='password'] {
-							border-color: $form_error_red;
-						}
-
-						span.invalid {
-							display: inline-block;
-						}
-					}
-				}
-
-				.button {
-					width: 100%;
-
-					button[type='submit'] {
-						width: 100%;
-						border: none;
-						cursor: pointer;
-						transition: all 200ms ease-in-out;
-						background-color: $blue;
-						border-radius: 0.3rem;
-						min-height: 4.5rem;
-
-						span {
-							font-size: 1.4rem;
-							color: white;
-							font-family: 'Arial CE', sans-serif;
-						}
-
-						&:hover {
-							filter: brightness(90%);
-						}
-					}
-				}
-
-				.sign_up_instead {
-					width: 100%;
-					text-align: left;
-
-					span {
+					label {
 						font-size: 1.3rem;
 						font-family: 'Arial CE', sans-serif;
-						color: $text-color-medium;
-						cursor: pointer;
+						color: $text-color-regular;
+					}
+
+					input[type='email'] {
+						padding: 0.5em;
+						border: 0.1rem solid $border-color-regular;
+						outline: none;
 						transition: all 200ms ease-in-out;
+						font-family: 'Arial CE', sans-serif;
+						border-radius: 0.3rem;
+
+						&::placeholder {
+							color: $text-color-medium;
+							font-family: 'Arial CE', sans-serif;
+						}
 
 						&:hover {
-							color: $blue;
+							box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;
 						}
+
+						&:focus {
+							box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;
+						}
+					}
+
+					span {
+						font-family: 'Arial CE', sans-serif;
+						font-size: 1.3rem;
+						font-weight: 500;
+						color: $form_error_red;
+						display: none;
+					}
+				}
+
+				.email_required {
+					input[type='email'] {
+						border-color: $form_error_red;
+					}
+
+					span.required {
+						display: inline-block;
+					}
+				}
+
+				.invalid_email {
+					input[type='email'] {
+						border-color: $form_error_red;
+					}
+
+					span.invalid {
+						display: inline-block;
+					}
+				}
+
+				.password {
+					display: flex;
+					flex-direction: column;
+					gap: 0.2em;
+					font-family: 'Arial CE', sans-serif;
+
+					label {
+						font-size: 1.3rem;
+						font-family: 'Arial CE', sans-serif;
+						color: $text-color-regular;
+					}
+
+					input[type='password'] {
+						padding: 0.5em;
+						border: 0.1rem solid $border-color-regular;
+						outline: none;
+						transition: all 200ms ease-in-out;
+						font-family: 'Arial CE', sans-serif;
+						border-radius: 0.3rem;
+
+						&::placeholder {
+							color: $text-color-medium;
+							font-family: 'Arial CE', sans-serif;
+						}
+
+						&:hover {
+							box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;
+						}
+
+						&:focus {
+							box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;
+						}
+					}
+
+					span {
+						font-family: 'Arial CE', sans-serif;
+						font-size: 1.3rem;
+						font-weight: 500;
+						color: $form_error_red;
+						display: none;
+					}
+				}
+
+				.password_required {
+					input[type='password'] {
+						border-color: $form_error_red;
+					}
+
+					span.required {
+						display: inline-block;
+					}
+				}
+
+				.invalid_password {
+					input[type='password'] {
+						border-color: $form_error_red;
+					}
+
+					span.invalid {
+						display: inline-block;
+					}
+				}
+			}
+
+			.button {
+				width: 100%;
+
+				button[type='submit'] {
+					width: 100%;
+					border: none;
+					cursor: pointer;
+					transition: all 200ms ease-in-out;
+					background-color: $blue;
+					border-radius: 0.3rem;
+					min-height: 4.5rem;
+
+					span {
+						font-size: 1.4rem;
+						color: white;
+						font-family: 'Arial CE', sans-serif;
+					}
+
+					&:hover {
+						filter: brightness(90%);
+					}
+				}
+			}
+
+			.sign_up_instead {
+				width: 100%;
+				text-align: left;
+
+				span {
+					font-size: 1.3rem;
+					font-family: 'Arial CE', sans-serif;
+					color: $text-color-medium;
+					cursor: pointer;
+					transition: all 200ms ease-in-out;
+
+					&:hover {
+						color: $blue;
 					}
 				}
 			}
 		}
 
-		.gif {
-			flex: 1;
-			height: 100%;
-			display: flex;
-			align-items: center;
-			justify-content: center;
-			background-color: white;
+		@media screen and (max-width: 768px) {
+			align-items: flex-start;
+			padding-top: 2em;
 
-			img {
-				object-fit: contain;
+			form {
+				width: 95%;
+			}
+		}
+
+		@media screen and (min-width: 769px) and (max-width: 1024px) {
+			flex-direction: column;
+
+			form {
+				width: 80%;
 			}
 		}
 	}
